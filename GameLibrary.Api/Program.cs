c3953ee078abc72f19d6e8b4cd0f33411728b7ca
Vitor <vicentebarbosavitor@gmail.com>
2025-06-30 22:58:41 -0300

    Remove HTTPS redirection and configure dynamic port
    Updated `Program.cs` to remove `app.UseHttpsRedirection()`.
    Added functionality to listen on a port specified by the
    environment variable "PORT", defaulting to "8080".
    Included a log message to indicate the port in use.
    Comments updated to reflect the order of authentication
    and authorization middleware.

--- GameLibrary.Api/Program.cs
+++ GameLibrary.Api/Program.cs
@@ -90,4 +90,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
+app.Logger.LogInformation($"Usando porta: {port}");
+
 app.Run();
